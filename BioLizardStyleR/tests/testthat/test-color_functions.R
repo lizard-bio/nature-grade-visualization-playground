0ccb874e76405f4193c4b78f692555e4816c0f96
aniko-meijer <aniko.meijer@gmail.com>
2025-01-09 16:30:45 +0100

    R: modify qualitative color scale + add hues and paired scale; issue #15

--- BioLizardStyleR/tests/testthat/test-color_functions.R
+++ BioLizardStyleR/tests/testthat/test-color_functions.R
@@ -2,16 +2,15 @@ library(ggplot2)
 
 data("mtcars")
 mtcars$gear <- as.factor(mtcars$gear)
+mtcars$carb <- as.factor(mtcars$carb)
 
 # Base plot
 testplot_discrete <- ggplot(data = mtcars, aes(x = hp, y = mpg)) +
-  geom_point(aes(color = gear, fill = gear), size=3, shape = 21) +
+  geom_point(aes(color = carb, fill = carb), size=3, shape = 21) +
   labs(
     title = "Miles per Gallon vs. Horsepower",
     x = "Horsepower",
-    y = "Miles per Gallon",
-    color = "Gears",
-    fill = "Gears"
+    y = "Miles per Gallon"
   )
 
 testplot_continuous <- ggplot(data = mtcars, aes(x = hp, y = mpg, color = mpg, fill = mpg)) +
@@ -26,11 +25,20 @@ testplot_continuous <- ggplot(data = mtcars, aes(x = hp, y = mpg, color = mpg, f
 
 test_that("biolizard_pal_qualitative works", {
   expect_length(biolizard_pal_qualitative(1), 1)
-  expect_length(biolizard_pal_qualitative(8), 8)
+  expect_length(biolizard_pal_qualitative(12), 12)
   expect_error(biolizard_pal_qualitative(13), regexp = 'number of colors exceeds')
   expect_error(biolizard_pal_qualitative(0), regexp = "at least 1")
   vdiffr::expect_doppelganger("discrete qualitative", testplot_discrete + scale_color_biolizard(type = "discrete", scheme = "qualitative") + scale_fill_biolizard(type = "discrete", scheme = "qualitative"))
   vdiffr::expect_doppelganger("discrete qualitative rev", testplot_discrete + scale_color_biolizard(type = "discrete", scheme = "qualitative", reverse=TRUE) + scale_fill_biolizard(type = "discrete", scheme = "qualitative", reverse=TRUE))
+})
+
+test_that("biolizard_pal_paired works", {
+  expect_length(biolizard_pal_paired(1), 1)
+  expect_length(biolizard_pal_paired(10), 10)
+  expect_error(biolizard_pal_paired(13), regexp = 'number of colors exceeds')
+  expect_error(biolizard_pal_paired(0), regexp = "at least 1")
+  vdiffr::expect_doppelganger("discrete paired", testplot_discrete + scale_color_biolizard(type = "discrete", scheme = "paired") + scale_fill_biolizard(type = "discrete", scheme = "paired"))
+  vdiffr::expect_doppelganger("discrete paired rev", testplot_discrete + scale_color_biolizard(type = "discrete", scheme = "paired", reverse=TRUE) + scale_fill_biolizard(type = "discrete", scheme = "paired", reverse=TRUE))
 })
 
 test_that("biolizard_pal_sequential works", {
