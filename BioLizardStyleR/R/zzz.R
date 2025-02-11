d68b566e2920da838bdd6d79fb4bb9e3c77f01b6
Artuur Couckuyt <artuurc@Artuurs-MacBook-Pro.local>
2025-02-11 10:05:14 +0100

    Added cache reset to check if Lato is installed after loading it

--- BioLizardStyleR/R/zzz.R
+++ BioLizardStyleR/R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage("Searching for Lato font...")
-  fonts<- systemfonts::system_fonts()
+  fonts <- systemfonts::system_fonts()
   if("Lato" %in% fonts$family){
     packageStartupMessage("Lato found! You're all set to start using the BLZ style!")
   } else {
@@ -10,13 +10,14 @@
     }, error = function(err) {
       stop(paste("sysfonts error: ", err, "\n\nTry installing the Lato font manually from the ttf files in https://github.com/lizard-bio/nature-grade-visualization-playground/tree/main/FontsToInstall and run `install_biolizard_fonts()`"))
     })
-    packageStartupMessage("Lato loaded! You're all set to start using the BLZ style!")
-    # # check if installation worked  --> does not work for some reason: systemfonts::system_fonts() not yet updated
-    # fonts<- systemfonts::system_fonts()
-    # if("Lato" %in% fonts$family){
-    #   packageStartupMessage("Lato loaded! You're all set to start using the BLZ style!")
-    # } else {
-    #   stop("Loading Lato unsuccesful: Try installing the Lato font manually from the ttf files in https://github.com/lizard-bio/nature-grade-visualization-playground/tree/main/FontsToInstall and run `install_biolizard_fonts()`")
-    # }
+    # packageStartupMessage("Lato loaded! You're all set to start using the BLZ style!")
+    # check if installation worked  --> does not work for some reason: systemfonts::system_fonts() not yet updated
+    systemfonts::reset_font_cache()
+    fonts <- systemfonts::system_fonts()
+    if("Lato" %in% fonts$family){
+      packageStartupMessage("Lato loaded! You're all set to start using the BLZ style!")
+    } else {
+      stop("Loading Lato unsuccesful: Try installing the Lato font manually from the ttf files in https://github.com/lizard-bio/nature-grade-visualization-playground/tree/main/FontsToInstall and run `install_biolizard_fonts()`")
+    }
   }
 }
