a6c05760216497c42b72ce589a32772f0a0f7abe
aniko-meijer <aniko.meijer@gmail.com>
2026-02-20 17:04:55 +0100

    R: deprecate l_viridis and update function descriptions

--- BioLizardStyleR/R/color_functions.R
+++ BioLizardStyleR/R/color_functions.R
@@ -53,16 +53,16 @@ blz_yellow <- {
 
 # color palette functions
 
-#' Biolizard qualitative 12-color palette
+#' Biolizard qualitative 8-color palette
 #'
 #' @description
-#' Returns a colorblind-safe qualitative color palette starting with Biolizard's signature green, blue and yellow.
+#' Returns a colorblind-safe, qualitative color palette featuring the 8 saturated colors from the BioLizard brand book.
 #'
 #' @examples
 #' biolizard_pal_qualitative(8)
 #' biolizard_pal_qualitative(4, reverse = TRUE)
 #'
-#' @param n Integer, number of colors. Maximum 12.
+#' @param n Integer, number of colors. Maximum 8.
 #' @param reverse Boolean, if TRUE, reverse the order of the colors
 #' @seealso [biolizard_pal_paired()] [biolizard_pal_hue()] [biolizard_pal_sequential()] [biolizard_pal_divergent()] [biolizard_pal_beige_blue()] [biolizard_pal_beige_gn_blue()]
 #' @export
@@ -85,7 +85,7 @@ biolizard_pal_qualitative <- function(n, reverse = FALSE) {
 #' Biolizard qualitative paired palette
 #'
 #' @description
-#' Returns a colorblind-safe qualitative color palette inspired by Biolizard's signature green, blue and yellow.
+#' Returns a colorblind-safe qualitative color palette featuring the colors in the Biolizard brand book.
 #' Every two consecutive colors have a similar hue, making this palette especially suitable for paired levels:
 #' e.g. two cases for different time points, such as control_t1, case_t1, control_t2, case_t2, control_t3, case_t3, ...
 #'
@@ -93,7 +93,7 @@ biolizard_pal_qualitative <- function(n, reverse = FALSE) {
 #' biolizard_pal_paired(6)
 #' biolizard_pal_paired(4, reverse = TRUE)
 #'
-#' @param n Integer, number of colors. Maximum 10.
+#' @param n Integer, number of colors. Maximum 6.
 #' @param reverse Boolean, if TRUE, reverse the order of the colors
 #' @seealso [biolizard_pal_hue()] [biolizard_pal_qualitative()] [biolizard_pal_sequential()] [biolizard_pal_divergent()] [biolizard_pal_beige_blue()] [biolizard_pal_beige_gn_blue()]
 #' @export
@@ -117,8 +117,8 @@ biolizard_pal_paired <- function(n, reverse = FALSE) {
 #'
 #' @description
 #' Specific case of the `scales::pal_hue` function that returns a discrete color scale
-#' starting with Biolizard's signature green (#01a086). This scale can be expanded to large
-#' number of colors, but DOES NOT guarantee color-blind safe palettes.
+#' starting with the dark green color in the brand book. This scale can be expanded to large
+#' number of colors, but DOES NOT guarantee colorblind-safe palettes.
 #'
 #' @examples
 #' biolizard_pal_hue(12)
@@ -141,7 +141,7 @@ biolizard_pal_hue <- function(n, reverse = FALSE) {
 #' Biolizard sequential palette
 #'
 #' @description
-#' Sequential color palette inspired by Biolizard's signature green.
+#' Sequential, colorblind-safe and perceptually uniform continuous color palette inspired by the light and darker shades of blue in the BioLizard brand book.
 #'
 #' @examples
 #' biolizard_pal_sequential(10)
@@ -168,7 +168,7 @@ biolizard_pal_sequential <- function(n, reverse = FALSE) {
 #' Biolizard divergent palette
 #'
 #' @description
-#' Divergent color palette inspired by Biolizard's signature green.
+#' Divergent, colorblind-safe and perceptually uniform (within each branch) continuous color palette inspired by the green-blue color gradient in the biolizard brand book.
 #'
 #' @examples
 #' biolizard_pal_divergent(10)
@@ -194,9 +194,12 @@ biolizard_pal_divergent <- function(n, reverse = FALSE) {
 }
 
 
-#' l_viridis continuous color scale TODO: deprecate
+#' l_viridis continuous color scale DEPRECATED
 #'
 #' @description
+#' `r lifecycle::badge("deprecated")`
+#' DEPRECATED. Use biolizard_pal_beige_blue(), biolizard_pal_beige_gn_blue() or biolizard_pal_sequential() instead. Returns values of biolizard_pal_beige_gn_blue().
+#'
 #' This color scale is inspired by the viridis colorscale, and passes through biolizard's signature yellow and green (or very similar).
 #' Like viridis it is colorblind-friendly and perceptually uniform.
 #' It was created using `viscm` (https://github.com/matplotlib/viscm) and named l_viridis, after the European green lizard (Lacerta viridis)
@@ -209,21 +212,13 @@ biolizard_pal_divergent <- function(n, reverse = FALSE) {
 #' @param reverse Boolean, if TRUE, reverse the order of the colors
 #' @importFrom scales pal_gradient_n
 #' @importFrom grDevices rgb
-#' @seealso [biolizard_pal_paired()] [biolizard_pal_hue()] [biolizard_pal_qualitative()] [biolizard_pal_divergent()]
+#' @seealso [biolizard_pal_beige_blue()] [biolizard_pal_beige_gn_blue()] [biolizard_pal_sequential()]
 #' @export
 #'
 biolizard_pal_l_viridis <- function(n, reverse = FALSE) {
-  if (n < 1) {
-    stop("The requested number of colors must be at least 1.")
-  }
-  # convert rgb colors from viscm to hex
-  hex <- apply(l_viridis_rgb, 1, function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 1))
-  colors <- pal_gradient_n(hex)(seq(0, 1, length.out = n))
-  if (reverse) {
-    return(colors)
-  } else {
-    return(rev(colors))  #reverse to make the palette start with yellow
-  }
+  lifecycle::deprecate_warn("3.0.0", what = "biolizard_pal_l_viridis()",
+                            details = "Please use 'biolizard_pal_beige_blue()', 'biolizard_pal_beige_gn_blue()' or 'biolizard_pal_sequential()' instead. Returning values from biolizard_pal_beige_gn_blue().")
+  biolizard_pal_beige_gn_blue(n, reverse)
 }
 
 #' beige-blue continuous scale
@@ -297,20 +292,25 @@ biolizard_pal_beige_gn_blue <- function(n, reverse = FALSE) {
 #' The function supports five schemes for discrete data:
 #'
 #' \itemize{
-#'   \item **qualitative**: Colorblind-safe qualitative color palette starting with Biolizard's signature green, blue and yellow.
-#'   \item **paired**: Colorblind-safe qualitative color palette inspired by Biolizard's signature colors, especially suited for levels that are related 2-by-2.
-#'   \item **hues**: Maps each level to an evenly spaced hue on the color wheel, starting with Biolizard's signature green. DOES NOT generate colorblind-safe palettes.
-#'   \item **l_viridis**: Sequential color palette inspired by the viridis color scale, passing through Biolizard's signature yellow and green.
-#'   \item **sequential**: Sequential color palette inspired by Biolizard's signature green.
-#'   \item **divergent**: Divergent color palette inspired by Biolizard's signature green.
+#'   \item **qualitative**: Colorblind-safe, qualitative color palette featuring the 8 saturated colors from the BioLizard brand book.
+#'   \item **paired**: Colorblind-safe qualitative color palette featuring the colors in the Biolizard brand book, especially suited for levels that are related 2-by-2.
+#'   \item **hues**: Maps each level to an evenly spaced hue on the color wheel, starting with the dark green from the brand book. DOES NOT generate colorblind-safe palettes.
+#'   \item **l_viridis**: `r lifecycle::badge("deprecated")` Use "beige_blue", "beige_gn_blue" or "sequential" instead. Returning colors from beige_gn_blue.
+#'   \item **sequential**: Sequential, colorblind-safe and perceptually uniform color palette inspired by the light and darker shades of blue in the BioLizard brand book.
+#'   \item **divergent**: Divergent, colorblind-safe and perceptually uniform (within each branch) color palette inspired by the green-blue color gradient in the biolizard brand book.
+#'   \item **beige_blue**: Continuous, perceptually uniform, color-blind friendly color scale inspired by the beige and blue colors in the brand book.
+#'   \item **beige_gn_blue**: Continuous, perceptually uniform, color-blind friendly color scale inspired by the beige, green and blue colors in the brand book.
 #' }
 #'
 #' And three schemes for continuous data:
 #'
 #' \itemize{
-#'   \item **l_viridis**: Sequential color palette inspired by the viridis color scale, passing through Biolizard's signature yellow and green.
-#'   \item **sequential**: Sequential color palette inspired by Biolizard's signature green.
-#'   \item **divergent**: Divergent color palette inspired by Biolizard's signature green.
+#'   \item **l_viridis**: `r lifecycle::badge("deprecated")` Use "beige_blue", "beige_gn_blue" or "sequential" instead. Returning colors from beige_gn_blue.
+#'   \item **sequential**: Sequential, colorblind-safe and perceptually uniform color palette inspired by the light and darker shades of blue in the BioLizard brand book.
+#'   \item **divergent**: Divergent, colorblind-safe and perceptually uniform (within each branch) color palette inspired by the green-blue color gradient in the biolizard brand book.
+#'   \item **beige_blue**: Continuous, perceptually uniform, color-blind friendly color scale inspired by the beige and blue colors in the brand book.
+#'   \item **beige_gn_blue**: Continuous, perceptually uniform, color-blind friendly color scale inspired by the beige, green and blue colors in the brand book.
+
 #' }
 #'
 #' @section Application:
@@ -341,7 +341,7 @@ biolizard_pal_beige_gn_blue <- function(n, reverse = FALSE) {
 #'       fill = "Miles per Gallon"
 #'     )
 #'  testplot +
-#'    scale_color_biolizard(type='continuous',scheme='l_viridis') +
+#'    scale_color_biolizard(type='continuous',scheme='sequential') +
 #'    lizard_style()
 #' }
 #'
@@ -356,10 +356,15 @@ biolizard_pal_beige_gn_blue <- function(n, reverse = FALSE) {
 #' @rdname biolizard_scales
 #' @export
 scale_color_biolizard <- function(type = "discrete",
-                                  scheme = ifelse(type == "discrete", "qualitative", "l_viridis"),
+                                  scheme = ifelse(type == "discrete", "qualitative", "sequential"),
                                   reverse = FALSE, ...) {
-  # convert l_viridis rgb colors from viscm to hex
-  l_viridis_hex <- apply(l_viridis_rgb, 1, function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 1))
+  if (scheme == "l_viridis") {
+    lifecycle::deprecate_warn("3.0.0", what = "scale_color_biolizard(scheme = 'l_viridis is deprecated')",
+                              details = "Please use scheme = 'beige_blue', 'beige_gn_blue' or 'sequential' instead. Returning values from beige_gn_blue.")
+  }
+
+  # convert rgb colors from viscm to hex
+  l_viridis_hex <- apply(beige_gn_blue_rgb, 1, function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 1))
   beige_blue_hex <- apply(beige_blue_rgb, 1, function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 1))
   beige_gn_blue_hex <- apply(beige_gn_blue_rgb, 1, function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 1))
 
@@ -368,7 +373,7 @@ scale_color_biolizard <- function(type = "discrete",
            qualitative = discrete_scale("colour", palette = function(n) biolizard_pal_qualitative(n, reverse), ...),
            paired = discrete_scale("colour", palette = function(n) biolizard_pal_paired(n, reverse), ...),
            hues = scale_color_hue(l = 55, c = 71, h.start = 135, direction = ifelse(reverse, -1, 1), ...),
-           l_viridis = discrete_scale("colour", palette = function(n) biolizard_pal_l_viridis(n, reverse), ...),
+           l_viridis = discrete_scale("colour", palette = function(n) biolizard_pal_beige_gn_blue(n, reverse), ...),
            sequential = discrete_scale("colour", palette = function(n) biolizard_pal_sequential(n, reverse), ...),
            divergent = discrete_scale("colour", palette = function(n) biolizard_pal_divergent(n, reverse), ...),
            beige_blue = discrete_scale("colour", palette = function(n) biolizard_pal_beige_blue(n, reverse), ...),
@@ -405,6 +410,11 @@ scale_colour_biolizard <- scale_color_biolizard
 #' @rdname biolizard_scales
 #' @export
 scale_fill_biolizard <- function(type = "discrete", scheme = ifelse(type == "discrete", "qualitative", "l_viridis"), reverse = FALSE, ...) {
+  if (scheme == "l_viridis") {
+    lifecycle::deprecate_warn("3.0.0", what = "scale_fill_biolizard(scheme = 'l_viridis')",
+                              details = "Please use scheme = 'beige_blue', 'beige_gn_blue' or 'sequential' instead. Returning values from beige_gn_blue.")
+  }
+
   # convert l_viridis rgb colors from viscm to hex
   l_viridis_hex <- apply(l_viridis_rgb, 1, function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 1))
   beige_blue_hex <- apply(beige_blue_rgb, 1, function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 1))
