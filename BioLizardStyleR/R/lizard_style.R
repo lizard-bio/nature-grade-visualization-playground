244f2dfbb1196e834b54978f8e5d7574c052f8d8
artuurC <couckuyt.artuur@gmail.com>
2026-02-25 12:17:59 +0100

    Add base color for bar geom in lizard_style

--- BioLizardStyleR/R/lizard_style.R
+++ BioLizardStyleR/R/lizard_style.R
@@ -25,7 +25,7 @@ set_default_BLZcolors <- function(){
 
    ggplot2::update_geom_defaults("boxplot", list(fill = base_fill_c, colour = base_element_c))
    ggplot2::update_geom_defaults("violin", list(fill = base_fill_c, colour = base_element_c))
-
+   ggplot2::update_geom_defaults("bar", list(fill = base_fill_c, colour = base_element_c))
    ggplot2::update_geom_defaults("rect", list(fill = base_fill_c, colour = base_element_c))
    ggplot2::update_geom_defaults("polygon", list(fill = base_fill_c, colour = base_element_c))
 }
